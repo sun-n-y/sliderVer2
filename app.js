25b9fe65017a8baa846f34c45f9c9fca51d1f35d
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-17 15:43:00 -0400

    add sliderver2 js position algo

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ import people from './data.js';
 const sliderContainer = document.querySelector('.slider-container');
 
 sliderContainer.innerHTML = people
-  .map((person) => {
+  .map((person, slideIndex) => {
     const { img, name, job, text } = person;
-    return `<article class="slide">
+    let position = 'next';
+    if (slideIndex === 0) {
+      position = 'active';
+    }
+    if (slideIndex === people.length - 1) {
+      position = 'last';
+    }
+    return `<article class="slide ${position}">
                 <img src="${img}" class="person-img" alt="${name}">
                 <h3 class="name">${name}</h3>
                 <h3 class="job">${job}</h3>
--- data.js
+++ data.js
@@ -17,6 +17,12 @@ const people = [
     job: 'designer',
     text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
   },
+  {
+    img: 'https://images.pexels.com/photos/894156/pexels-photo-894156.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name: 'jermih Oui',
+    job: 'singer',
+    text: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem quoeius recusandae officia voluptas sint deserunt dicta nihil nam omnis?`,
+  },
 ];
 
 export default people;
